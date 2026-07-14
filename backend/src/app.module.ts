6298c67d835b7ee65251958724e8e8b3cf84c933
manhleduy <mle529189@gmail.com>
2026-07-14 09:52:07 +0700

    define prisma schema file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path/win32';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
